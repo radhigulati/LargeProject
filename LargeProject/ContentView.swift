c7beea58cbf7f9fb90c725836d045bdf6f3850cc
Radhika Gulati <radhigulati@gmail.com>
2022-02-03 13:18:46 -0800

    Initial Commit

---
+++ LargeProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LargeProject
+//
+//  Created by Radhika Gulati on 2/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LargeProject/LargeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  LargeProjectApp.swift
+//  LargeProject
+//
+//  Created by Radhika Gulati on 2/3/22.
+//
+
+import SwiftUI
+
+@main
+struct LargeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
